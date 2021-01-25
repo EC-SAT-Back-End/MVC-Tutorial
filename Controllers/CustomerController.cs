4a7b9fc0cf1413629aa85fd738bb39706023b21f
Ahmed Break <Ahmed.Break@ec-sat.local>
2021-01-25 15:35:11 +0200

    8 - Razor View Engine - Working with Model in ASP.NET MVC

---
+++ Controllers/CustomerController.cs
@@ -0,0 +1,20 @@
+﻿using MVC_Tutoriall.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC_Tutoriall.Controllers
+{
+    public class CustomerController : Controller
+    {
+        // GET: Customer
+        public ActionResult DisplayCustomer()
+        {
+            var customer = new Customer(100, "ahmed", "cairo", 32);
+
+            return View(customer);
+        }
+    }
+}
